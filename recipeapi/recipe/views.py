a048ba9ac429c21212d949d57dc012dc8dc4f074
smagucha <smagucha@gmail.com>
2021-03-11 16:53:38 +0300

    search working

--- recipeapi/recipe/views.py
+++ recipeapi/recipe/views.py
@@ -7,37 +7,23 @@ def home(request):
 	responseapi = requests.request("GET", apikey.url)
 	r= responseapi.json()
 	json_object = json.dumps(r)
-	# hits=r['hits']
-	recipe= r['hits'][0]['recipe']
-	# recipelabel1=r['hits'][9]['recipe']['label']
-	# recipeimage=r['hits'][9]['recipe']['image']
-	# recipeingredientLines=r['hits'][9]['recipe']['ingredientLines']
-
-	# for recipe in hits:
-	# 	print('this is recipe  ...........', recipe['recipe'],  end='\n',)
+	hits=r['hits']
 	
-	# for i in hits:
-	# 	print(i['recipe']['label'], end='')
-	# for r in recipe:
-	# 	print(f"{r['recipe']['label']}- ingredients:{r['recipe']['ingredientLines'][0]}")
-	# for r in recipe:
-	# 	print(f"{r['recipe']['label']}- ingredients:{r['recipe']['ingredientLines']}")
-
 	newrecipe=[]
-	for i in recipe:
+
+	for recipe in hits:
 		newrecipeska={
-			'recipelabel':r['hits'][0]['recipe']['label'],
-			'recipeimage':r['hits'][0]['recipe']['image'],
-			'recipeingredientLines':r['hits'][0]['recipe']['ingredientLines'][0],
+			'recipelabel':recipe['recipe']['label'],
+			'recipeimage':recipe['recipe']['image'],
+			'recipeingredientLines':recipe['recipe']['ingredientLines'],
 		}
 		newrecipe.append(newrecipeska)
-	return render(request, 'recipe/home.html',{'newrecipe':newrecipe})
+	context ={
+		'newrecipe':newrecipe
+	}
+	return render(request, 'recipe/home.html', context)
 		
 
 def Ingendrients(request):
-	x =12345
-	context={
-	'y':x
-
-	}
+	
 	return render(request, 'recipe/ingredientLines.html',context)
